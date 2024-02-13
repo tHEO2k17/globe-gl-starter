b06f316504ef075a0227dfd25a62ef1075228cc2
Theophilus Paintsil <theophilus.paintsil@tech11.com>
2024-02-13 17:43:08 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Globe from 'react-globe.gl';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [countries, setCountries] = useState({ features: []});
+
+    useEffect(() => {
+      // load data
+      fetch("./ne_110m_admin_0_countries.geojson")
+        .then((res) => {
+          console.log('res :>> ', res);
+          return res.json();
+        })
+        .then((fetchedCountries) => {
+          console.log("fetchedCountries :>> ", fetchedCountries);
+          setCountries(fetchedCountries);
+        });
+    }, []);
+
+    return (
+      <Globe
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-water.png"
+        hexPolygonsData={countries.features}
+        hexPolygonResolution={3}
+        hexPolygonMargin={0.3}
+        hexPolygonUseDots={true}
+        backgroundColor="#FFF"
+        onZoom={() => false}
+        hexPolygonColor={({properties : d}: any) => d.POLYGON_COLOR || "#FFF"}
+        // hexPolygonColor={() =>
+        //   `#${Math.round(Math.random() * Math.pow(2, 24))
+        //     .toString(16)
+        //     .padStart(6, "0")}`
+        // }
+        hexPolygonLabel={({ properties: d }: any) => `
+        <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
+        Population: <i>${d.POP_EST}</i>
+      `}
+      />
+    );
+
 }
 
 export default App;
